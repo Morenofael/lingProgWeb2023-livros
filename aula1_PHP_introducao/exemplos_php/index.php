82110cb45a1347753617981cd2d737f0ba092015
Daniel Di Domenico <ddomenico@inf.ufpel.edu.br>
2023-03-15 15:07:56 -0300

    Exercícios PHP - Aula 1

---
+++ aula1_PHP_introducao/exemplos_php/index.php
@@ -0,0 +1,4 @@
+<?php
+    //print "Olá mundo!";
+    echo "Olá mundo!";
+?>
---
+++ aula1_PHP_introducao/exemplos_php/var.php
@@ -0,0 +1,8 @@
+<?php
+    $aula = "Aula PHP do dia 10/03/2023";
+    echo $aula;
+
+    $num = 123;
+    
+    print "<br>" . $num;
+?>
